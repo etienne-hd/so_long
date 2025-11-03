8b9352f976093915bb82cb62ed9e95ab53e6b91d
etienne-hd <hode.etienne@gmail.com>
2025-11-03 05:00:39 +0100

    Removed: libft.a Added: get_info function for map window info

--- srcs/game/game.c
+++ srcs/game/game.c
@@ -6,11 +6,12 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 19:36:10 by ehode             #+#    #+#             */
-/*   Updated: 2025/11/02 09:00:10 by ehode            ###   ########.fr       */
+/*   Updated: 2025/11/03 04:51:20 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include <stddef.h>
 
 static void	destroy(t_game *game)
 {
@@ -19,10 +20,23 @@ static void	destroy(t_game *game)
 	mlx_destroy_context(game->mlx);
 }
 
+static mlx_window_create_info	get_info(size_t x, size_t y)
+{
+	mlx_window_create_info	info;
+
+	info.is_fullscreen = 0;
+	info.render_target = 0;
+	info.title = "so_long";
+	info.width = x;
+	info.height = y;
+	info.is_resizable = false;
+	return (info);
+}	
+
 void	start_game(t_map *map, t_player *player)
 {
 	t_game					game;
-	mlx_window_create_info	info = {0};
+	mlx_window_create_info	info;
 
 	game.map = map;
 	game.player = player;
@@ -34,10 +48,7 @@ void	start_game(t_map *map, t_player *player)
 		destroy(&game);
 		return ;
 	}
-	info.title = "so_long";
-	info.width = map->size.x * TEXTURE_SIZE;
-	info.height = map->size.y * TEXTURE_SIZE;
-	info.is_resizable = false;
+	info = get_info(map->size.x * TEXTURE_SIZE, map->size.y * TEXTURE_SIZE);
 	game.win = mlx_new_window(game.mlx, &info);
 	mlx_set_fps_goal(game.mlx, 30);
 	mlx_on_event(game.mlx, game.win, MLX_KEYDOWN, game_key_hook, &game);
